aa09a5533a83bae33b14e98c87b6c85001b3b4fb
Danyl Demchenko <danyldemchenko.m@gmail.com>
2026-08-16 02:05:47 +0300

    fix JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const contactForm = document.querySelector('.contacts__form');
+
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  contactForm.reset();
+});
